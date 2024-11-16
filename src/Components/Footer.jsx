cc95b5c64d211dcac5bc60f7af0af5e875ede39c
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2024-11-16 11:36:02 +0100

    Footer y headers hechos

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,10 +3,14 @@ import {NavLink} from "react-router-dom";
 
 const Footer = () => {
     return (
-        <footer>
-            <NavLink to={"/contact_me"}>Contactar</NavLink>
+        <footer className="footer fixed-bottom ">
+            <nav className="navbar navbar-expand-lg bg-mi-color text-descriptivo w-100">
+                <NavLink to={"/contact_me"}>Contactar</NavLink>
+            </nav>
+
         </footer>
     );
 };
 
 export default Footer;
+
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,14 +4,27 @@ import {NavLink} from "react-router-dom";
 const Header = () => {
     return (
         <header>
-
-            <ul>
-                <img src={"src/assets/mYmelody rate.svg"} alt={"Logo"} style={{height: "85px"}}/>
-                <input placeholder={"Busca un artista,album,cancion..."}/>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-            </ul>
+            <nav className="navbar navbar-expand-lg bg-mi-color text-descriptivo w-100">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        <img className={"img-sin-fondo"} src={"src/assets/mYmelody rate.svg"} alt={"Logo"} style={{height: "100px"}}/>
+                    </a>
+                    <input
+                        className="form-control me-2 bg-input"
+                        type="search"
+                        placeholder="Busca un artista, álbum, canción..."
+                        aria-label="Search"
+                    />
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <NavLink className="nav-link text-white" to="/about">About</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link text-white" to="/contact_me">Contactar</NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </header>
     );
 };
