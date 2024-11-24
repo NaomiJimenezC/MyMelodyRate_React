13da262d5b45550f893593de049b9427d76879dc
NaomiJimenezC <ijimenez@iesrafaelalberti.es>
2024-11-24 23:21:39 +0100

    implementación de la pi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,86 @@
 import React from 'react';
+import Card from "../Components/Card.jsx";
+import CardArtist from "../Components/CardArtist.jsx";
+
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+    const [artists, setArtists] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchArtists = async () => {
+            try {
+                const fetchedArtists = await popularArtist();
+                setArtists(fetchedArtists);
+            } catch (error) {
+                console.error('Error:', error);
+                setError('Error al cargar los artistas');
+            }
+        };
+
+        fetchArtists();
+    }, []);
+
     return (
-        <div>
-            home
-        </div>
+        <>
+            {getTopArtist()}
+            <section className="canciones">
+                {error ? (
+                    <p>{error}</p>
+                ) : (
+                    artists.map((artista, index) => (
+                        <article key={index}>
+                            <img src={artista.image[0]['#text']} alt={artista.name}/>
+                            <p>{artista.name}</p>
+                        </article>
+                    ))
+                )}
+            </section>
+
+            <section className="canciones">
+                <Card/>
+                <Card/>
+                <Card/>
+            </section>
+            <section className="artistas">
+                <CardArtist/>
+                <CardArtist/>
+                <CardArtist/>
+            </section>
+        </>
     );
 };
 
 export default Home;
+
+
+export const getTopTracks = () => {
+    //https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&api_key=${import.meta.env.VITE_LAST_FM_API_KEY}&format=json
+    fetch(`https://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=spain&api_key=${import.meta.env.VITE_LAST_FM_API_KEY}&format=json`)
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error fetching toptracks tracks:', error));
+}
+
+const getTopArtist = () => {
+    fetch(`https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&api_key=${import.meta.env.VITE_LAST_FM_API_KEY}&format=json`)
+        .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+export const popularArtist = async () => {
+    try {
+        const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=spain&api_key=${import.meta.env.VITE_LAST_FM_API_KEY}&format=json`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return (data.topartists.artist) ;
+    } catch (error) {
+        console.error('Error fetching popular artists:', error);
+        throw error;
+    }
+};
